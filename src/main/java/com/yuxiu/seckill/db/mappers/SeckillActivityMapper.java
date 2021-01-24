1f4b602d097292e1d065280efbda86187c6ac963
yuxiushi <shiyuxiu2013@gmail.com>
2021-01-24 13:34:14 -0800

    First commit for  flash sell

---
+++ src/main/java/com/yuxiu/seckill/db/mappers/SeckillActivityMapper.java
@@ -0,0 +1,17 @@
+package com.yuxiu.seckill.db.mappers;
+
+import com.yuxiu.seckill.db.po.SeckillActivity;
+
+public interface SeckillActivityMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(SeckillActivity record);
+
+    int insertSelective(SeckillActivity record);
+
+    SeckillActivity selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(SeckillActivity record);
+
+    int updateByPrimaryKey(SeckillActivity record);
+}
